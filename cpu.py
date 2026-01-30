d7b22b34a0ba61a6a7b859f8c270de573500c984
cranban <yanis.benmouhoub.rd@gmail.com>
2026-01-29 20:12:49 -0800

    Simplified code and fixed bugs

--- cpu.py
+++ cpu.py
@@ -1,50 +1,57 @@
 def g_not(a):
-    return ~a
+    return a^1
 
-def g_and(a,b):
-    return a&b
+def g_and(a, b):
+    return a & b
 
-def g_or(a,b):
-    return a|b
+def g_or(a, b):
+    return a | b
 
-def g_xor(a,b):
-    return a^b
+def g_xor(a, b):
+    return a ^ b
 
-def g_nor(a,b):
-    return ~(a|b)
+def g_nor(a, b):
+    return (a | b)^1
 
-def g_nand(a,b):
-    return ~(a&b)
+def g_nand(a, b):
+    return (a & b)^1
 
-def g_xnor(a,b):
-    return ~(a^b)
+def g_xnor(a, b):
+    return (a ^ b)^1
 
 while True:
-    usr=input("Enter two numbers along with the logic gate you want to use in the following format: a b gate\n")
-    print("To quit type exit")
-    x=usr.split()
-    
-    if x[2]=="not":
-        print(g_not(bin(int(x[0])),0))
-    
-    elif x[2]=="and":
-        print(g_and(bin(int(x[0])),bin(int(x[1]))))
-    
-    elif x[2]=="or":
-        print(g_or(bin(int(x[0])),bin(int(x[1]))))
-    
-    elif x[2]=="xor":
-        print(g_xor(bin(int(x[0])),bin(int(x[1]))))
-    
-    elif x[2]=="nor":
-        print(g_nor(bin(int(x[0])),bin(int(x[1]))))
-    
-    elif x[2]=="nand":
-        print(g_nand(bin(int(x[0])),bin(int(x[1]))))
-    
-    elif x[2]=="xnor":
-        print(g_xnor(bin(int(x[0])),bin(int(x[1]))))
-       
-    
-    
-    
+    usr = input(
+        "Enter two numbers and a logic gate in the format: a b gate\n"
+        "To quit type exit\n"
+    )
+
+    if usr == "exit":
+        break
+
+    x = usr.split()
+
+    a = int(x[0])
+    b = int(x[1])
+    gate = x[2]
+
+    if gate == "not":
+        print(g_not(a))
+
+    elif gate == "and":
+        print(g_and(a, b))
+
+    elif gate == "or":
+        print(g_or(a, b))
+
+    elif gate == "xor":
+        print(g_xor(a, b))
+
+    elif gate == "nor":
+        print(g_nor(a, b))
+
+    elif gate == "nand":
+        print(g_nand(a, b))
+
+    elif gate == "xnor":
+        print(g_xnor(a, b))
+
