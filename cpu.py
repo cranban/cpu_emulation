ed878c416b8de2de95b71aa4a9e486c337a05e6d
cranban <yanis.benmouhoub.rd@gmail.com>
2026-02-05 13:57:11 -0800

    Add files via upload
    Expanded the instruction set I'm having trouble implementing indirect memory addressing.

--- cpu.py
+++ cpu.py
@@ -5,14 +5,20 @@ storea = 0x3
 storeb = 0x4
 add = 0x5
 jmp = 0x6
+inp = 0x7
+out = 0x8
+mov = 0x9
 halt = 0xf
 
 reg_a=0
 reg_b=0
+inpr=0
+outr=0
+par=0x00
 pc=0
 running=True
 memory=256*[0]
-print(memory)
+
 
 def fetch():
     global pc
@@ -21,12 +27,11 @@ def fetch():
     return instruction
 
 def execute(instruction):
-    global pc, reg_a, reg_b , running 
-    print(f"instruction:{instruction}")
+    global pc, reg_a, reg_b, inpr, outr, par, running 
+    
     opcode=instruction>>4
-    operand=instruction & 0x0f
-    print(f"opcode:{opcode}")
-    print(f"operand:{operand}")
+    operand=instruction&0x0f
+    
     if opcode==nop:
         pass
     elif opcode==loada:
@@ -37,20 +42,28 @@ def execute(instruction):
         memory[operand]=reg_a
     elif opcode==storeb:
         memory[operand]=reg_b
+    elif opcode==mov:
+        memory[operand]
     elif opcode==add:
         reg_a=reg_a+reg_b
-        print(f"sum:{reg_a}")
     elif opcode==jmp:
         pc=operand
+    elif opcode==inp:
+        inpr=int(input())
+    elif opcode==out:
+        outr=memory[par]
+        print(outr)
     elif opcode==halt:
         running=False
+
 
 memory[0]=(loada<<4)|0x4
 memory[1]=(loadb<<4)|0x8
 memory[2]=(add<<4)|0x0
-memory[3]=(storea<<4)|0x9
+memory[3]=(storea<<4)|0xf
 memory[4]=(halt<<4)|0x0
 
 while running==True:
     instruction=fetch()
     execute(instruction)
+    print(memory)
