01ebfdc06d4b83354fcf328ba82705ca8ac07544
cranban <yanis.benmouhoub.rd@gmail.com>
2026-02-24 13:20:48 -0800

    Add files via upload
    restarted it but with more memory and instructions

--- cpu.py
+++ cpu.py
@@ -1,129 +1,74 @@
-#instruction set
-nop = 0x00 
-loada = 0x01
-loadb = 0x02
-storea = 0x03
-storeb = 0x04
-addi = 0x05
-jmp = 0x06
-inp = 0x07
-out = 0x08
-mov = 0x09
-loadi = 0x0a
-storei = 0x0b
-shiftl = 0x0c
-shiftr = 0x0d
-jz = 0x0e
-sub = 0x0f
-halt = 0x10
-
+#instructions
+lda=0x00
+ldb=0x01
+ldc=0x02
+ldi=0x03
+sta=0x04
+stb=0x05
+stc=0x06
+sti=0x07
+mov=0x08
+add=0x09
+sub=0x0a
+mul=0x0b
+div=0x0c
+gand=0x0d
+gor=0x0e
+gxor=0x0f
+shl=0x10
+shr=0x11
+cmp=0x12
+dec=0x13
+inc=0x14
+jmp=0x15
+jz=0x16
+jnz=0x17
+jn=0x18
+jc=0x19
+push=0x1a
+pop=0x1b
+call=0x1c
+ret=0x1d
+nop=0x1e
+halt=0x1f
+memory=[0]*65536
 #registers
-reg_a = 0           #id:0x0
-reg_b = 0           #id:0x1
-inpr = 0            #id:0x2
-outr = 0            #id:0x3
-indr = 0            #id:0x4
-pc = 0              
-zf = 0              
-nf = 0
-running = True
-memory = 256 * [0]
+reg_a=0
+reg_b=0
+reg_c=0
+reg_out=0
+zero_flag=0
+negative_flag=0
+carry_flag=0
+program_counter=0
+stack_pointer=0
+reg_instruction=0
+reg_index=0
+running=True
 
-#fetch decode and execute
-def fetch():
-    global pc
-    instruction = memory[pc]
-    pc= (pc + 1) & 0xff
-    return instruction
+def fetch(opcode,operand):
+    global program_counter
+    opcode=memory[program_counter]
+    lowbyte=memory[program_counter+1]
+    highbyte=memory[program_counter+2]
+    operand=(highbyte<<8)|lowbyte
+    program_counter=(program_counter+3)&0xffff
+    return opcode,operand
 
-def execute(instruction):
-    global pc, zf, reg_a, reg_b, inpr, outr, indr, running 
+def execute(opcode,operand):
+    global reg_a,reg_b,reg_c,reg_out,zero_flag,negative_flag,
+    carry_flag,program_counter,stack_pointer,reg_instruction,
+    reg_index,running
     
-    opcode = instruction >> 4
-    operand = instruction & 0x0f
-    
-    if opcode == nop:
-        pass
-    
-    elif opcode == loada:
-        memory[indr]=reg_a
-    
-    elif opcode == loadb:
-        memory[indr]=reg_b
-    
-    elif opcode == storea:
-        reg_a = memory[indr]
-    
-    elif opcode == storeb:
-        reg_b = memory[indr]
-    
-    elif opcode == mov:
-        origin=operand>>2
-        destination=operand&0xf
-        if origin==0x0 and destination==0x1:
-            reg_b=reg_a
-        elif origin==0x0 and destination==0x2:
-            inpr=reg_a
-        elif origin==0x0 and destination==0x3:
-            outr=reg_a
-        elif origin==0x0 and destination==0x4:
-            indr=reg_a
-        elif origin==0x1 and destination==0x0:
-            reg_a=reg_b
-        elif origin==0x1 and destination==0x2:
-            inpr=reg_b
-        elif origin==0x1 and destination==0x3:
-            outr=reg_b
-        elif origin==0x1 and destination==0x4:
-            indr=reg_b
-        elif origin==0x2 and destination==0x0:
-            reg_a=inpr
+    if opcode==lda:
+        reg_a=memory[reg_index]
+    elif opcode==ldb:
+        reg_b=memory[reg_index]
+    elif opcode==ldc:
+        reg_c=memory[reg_index]
+    elif opcode==ldi:
+        reg_a=operand
 
-                
-    elif opcode == addi:
-        reg_a = (reg_a + reg_b) & 0xff
-    
-    elif opcode = subi:
-        reg_a = (reg_a - reg_b)
-        if reg_a==0:
-            zf=1
-        elif reg_a<0:
-            nf=1
-    
-    elif opcode == jmp:
-        pc = operand
-    
-    elif opcode == inp:
-        inpr = int(input()) & 0xff
-    
-    elif opcode == out:
-        outr = memory[indr]
-        print(outr)
-    
-    elif opcode == loadi:
-        reg_a = memory[indr]
-    
-    elif opcode == storei:
-        memory[indr] = reg_a
-    
-    elif opcode == shiftr:
-        indr = (indr << operand) & 0xff
-    
-    elif opcode == shiftl:
-        indr = (indr>>operand) & 0xff
-    
-    elif opcode == jz and zf ==1:
-        pc = indr
-    
-    elif opcode == sub:
-        reg_a = (reg_a - reg_b) & 0xff
-    
-    elif opcode==halt:
-        running=False
-
-#program
-memory[0]=mov|((0x0<<2)|0x1)
-while running == True:
-    instruction = fetch()
-    execute(instruction)
-    print(memory)
+while running==True:
+    opcode,operand=fetch(opcode,operand)
+    execute(opcode,operand)
